00dc420889366355633211255e8e6109158fe66a
Cheuk Tung Ho <ChuckHo555@gmail.com>
2025-02-25 12:02:59 -0500

    Created Question Model and Questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,49 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    "Why did Goku go to the store?", 
+    [
+      "Because he needed more Saiyan seasoning",
+      "Because he ran out of Senzu beans",
+      "Because Chi-Chi told him to",
+      "Because he wanted to fight the cashier"
+    ]),
+    
+QuizQuestion(
+    "Why did Luffy bring a ladder to the Grand Line?", 
+    [
+      "Because he wanted to reach new heights",
+      "Because he heard the One Piece was on the roof",
+      "Because he needed to climb up to the crow's nest",
+      "Because Zoro kept getting lost below deck"
+    ]),
+
+QuizQuestion(
+    "Why did Sasuke refuse to play hide and seek?", 
+    [
+      "Because he never wanted to be found",
+      "Because Itachi was too good at hiding",
+      "Because Naruto always used Shadow Clones",
+      "Because he was too edgy for games"
+    ]),
+
+QuizQuestion(
+    "Why did Light Yagami fail art class?", 
+    [
+      "Because he couldn't draw the line between good and evil",
+      "Because he only drew names",
+      "Because he kept writing in a notebook instead",
+      "Because Ryuk kept distracting him"
+    ]),
+
+QuizQuestion(
+    "Why did Zenitsu run away from the battlefield?", 
+    [
+      "Because he wanted to practice his breathing… heavily",
+      "Because he thought a demon was actually his grandma",
+      "Because he was saving his energy for a nap",
+      "Because Nezuko wasn't there to impress"
+    ])
+
+];
